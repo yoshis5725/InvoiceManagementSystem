502dba199369c927739c2d2e9e9f9f605c6c99f3
elliottp <elliottpolite@gmail.com>
2022-07-06 12:40:50 -0600

    created the pages/invoices directory and scattfolded the razor pages

--- IdentityApp/Data/ApplicationDbContext.cs
+++ IdentityApp/Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using IdentityApp;
 
 namespace IdentityApp.Data;
 
@@ -9,4 +10,5 @@ public class ApplicationDbContext : IdentityDbContext
         : base(options)
     {
     }
+    public DbSet<IdentityApp.Invoice>? Invoice { get; set; }
 }
